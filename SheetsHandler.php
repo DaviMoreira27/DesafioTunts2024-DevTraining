b55e056de4e74a01547500a7eb9e9bc5fbc989d6
DaviMoreira27 <davimsantana2706@gmail.com>
2024-02-09 17:33:37 -0300

    Tradução de comentários e DOCBlocks

--- SheetsHandler.php
+++ SheetsHandler.php
@@ -10,21 +10,53 @@ use Google\Service\Exception;
 use Google\Service\Sheets as GoogleSheets;
 use GuzzleHttp\Client as HttpHandler;
 
+
+/**
+ * Enviroment variable, necessary to authorize the API acess.
+ */
+
 putenv('GOOGLE_APPLICATION_CREDENTIALS=config.json');
 
 class SheetsHandler
 {
 
+    /**
+     * The sheet id that you can get from the URL.
+     * 
+     * @param string
+     */
+
     private string $sheetId;
+
+    /**
+     * The sheet cell or column interval. Defines the region that the values are gonna be obtained.
+     * 
+     * @param string
+     */
     private string $rangeSheet;
 
+
+
+    /**
+     * Contructor
+     * 
+     * @param string
+     * @param string
+     * 
+     */
     public function __construct(string $sheetId, string $rangeSheet)
     {
         $this->sheetId = $sheetId;
         $this->rangeSheet = $rangeSheet;
     }
 
-    public function connectToGoogle(): Client
+    /**
+     * Makes the connection with the Google API.
+     * 
+     * @return Client|Exception
+     */
+
+    public function connectToGoogle(): Client|Exception
     {
         $client = new Client();
 
@@ -42,24 +74,35 @@ class SheetsHandler
         return $client;
     }
 
+    /**
+     * Get the given cell or column interval
+     * 
+     * @return Object|bool
+     */
 
-    public function getRowsFromSheet()
+    public function getRowsFromSheet(): Object|bool
     {
         $service = new GoogleSheets($this->connectToGoogle());
         $result = $service->spreadsheets_values->get($this->sheetId, $this->rangeSheet);
-        
-        if($result){
+
+        if ($result) {
             return $result;
         }
 
         return false;
     }
 
+    /**
+     * Handle the received results
+     *
+     * @return array
+     */
 
-    public function handleRowsResult(){
+    public function handleRowsResult()
+    {
         $result = $this->getRowsFromSheet();
 
-        if(!$result){
+        if (!$result) {
             return "Ocorreu um erro ao obter os valores!";
         }
 
--- Student.php
+++ Student.php
@@ -10,8 +10,25 @@ require_once "./SheetsHandler.php";
 class Student
 {
 
+    /**
+     * The sheet id that you can get from the URL.
+     * @param string
+     */
+
     private string $sheetId;
+
+    /**
+     * The sheet cell or column interval. Defines the region that the values are gonna be obtained.
+     * 
+     * @param string
+     */
     private string $rangeSheet;
+
+    /**
+     * The total of classes given.
+     * 
+     * @param int
+     */
     private int $maximumClasses = 60;
 
     public function __construct(string $sheetId, string $rangeSheet)
@@ -20,10 +37,23 @@ class Student
         $this->rangeSheet = $rangeSheet;
     }
 
+    /**
+     * Retrieve the data, calling the connection method.
+     * 
+     * @return array
+     */
+
     public function getData(): array
     {
         return (new SheetsHandler($this->sheetId, $this->rangeSheet))->handleRowsResult();
     }
+
+
+    /**
+     * Change the array, replacing the array key, for a more readable form.
+     * 
+     * @return array
+     */
 
     public function changedArray(): array
     {
@@ -46,6 +76,11 @@ class Student
         return $newArray;
     }
 
+    /**
+     * Calculates the average value from the 3 semesters, changing the student situation.
+     * 
+     * @return array
+     */
 
     public function calcAverage(): array
     {
@@ -80,13 +115,20 @@ class Student
         return $newArray;
     }
 
-    public function checkAbsence() : array{
+    /**
+     * Checks the student absence, making a new change in the situation.
+     * 
+     * @return array
+     */
+
+    public function checkAbsence(): array
+    {
         $calcStudents = $this->calcAverage();
-        $maximumPercent = (25/100) * $this->maximumClasses;
+        $maximumPercent = (25 / 100) * $this->maximumClasses;
         $newArray = [];
 
         foreach ($calcStudents as $key => $value) {
-            if($value['faltas'] > $maximumPercent){
+            if ($value['faltas'] > $maximumPercent) {
                 $value['situacao'] = 'Reprovado por falta';
             }
 
@@ -96,14 +138,21 @@ class Student
         return $newArray;
     }
 
-    public function checkLackOfGrade() : array{
+    /**
+     * At last, searches for the students that do not have the necessary grade and calculates that.
+     * 
+     * @return array
+     */
+
+    public function checkLackOfGrade(): array
+    {
         $newArray = [];
-        
+
         foreach ($this->checkAbsence() as $key => $value) {
             $minimumGrade = 100 - $value['media'];
-            if($value['situacao'] == 'Exame Final'){
+            if ($value['situacao'] == 'Exame Final') {
                 $value['naf'] = round($minimumGrade);
-            }else{
+            } else {
                 $value['naf'] = 0;
             }
 
